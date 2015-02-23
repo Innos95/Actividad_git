f88abe47fd231eda91359cc50437f633dc6f0a97
Innos95 <innosvpn@gmail.com>
2015-02-23 20:50:49 +0100

    Update YEhe.java

--- ProyectoGit/src/git/YEhe.java
+++ ProyectoGit/src/git/YEhe.java
@@ -4,6 +4,7 @@ public class YEhe {
 
 	public static void main(String[] args) {
 		//primera prueba con github
+		//segunda prueba con github
 		System.out.println("GitHub es una buena forma de compartir proyectos.");
 		System.out.println("Aunque hay que pagar T_T");
 	}
