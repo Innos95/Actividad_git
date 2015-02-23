73e898c84330870586eff9cce50b4d5bbbe36052
Innos95 <innosvpn@gmail.com>
2015-02-23 20:32:24 +0100

    Primer commit

---
+++ ProyectoGit/src/git/YEhe.java
@@ -0,0 +1,10 @@
+package git;
+
+public class YEhe {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub es una buena forma de compartir proyectos.");
+		System.out.println("Aunque hay que pagar T_T");
+	}
+
+}
